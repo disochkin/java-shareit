22d7b8ea1f8f631ea6d5f0e5c1224b312a5d7458
Denis Nagovitsyn <disochkin@gmail.com>
2025-11-12 22:15:47 +0500

    disable tests

--- server/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
