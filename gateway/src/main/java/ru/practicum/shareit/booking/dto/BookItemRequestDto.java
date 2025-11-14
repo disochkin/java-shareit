1055edbca05d61e58c5ed9fedb5e63a840399124
Denis Nagovitsyn <disochkin@gmail.com>
2025-11-14 12:05:03 +0500

    fix

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +11,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BookItemRequestDto {
     private long itemId;
-    @FutureOrPresent
     private LocalDateTime start;
-    @Future
     private LocalDateTime end;
 }
